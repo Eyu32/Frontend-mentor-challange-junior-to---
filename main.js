03f13f7a29d9186aed9a147a69a18401b6f0c876
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-03-16 22:30:25 +0300

    Updated feature XYZ or fixed issue ABC

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const img = document.querySelector('#btn-image')
 const url = "https://api.adviceslip.com/advice"
 
 const getQuote = () => {
-    img.classList.add('rotate')
+    // img.classList.add('rotate')
     quoteContainer.classList.remove('fade')
     fetch(url)
     .then(data => data.json())
@@ -14,7 +14,7 @@ const getQuote = () => {
         quoteContainer.classList.remove('fade')
         quoteId.textContent = `#${item.slip.id}`
         quoteContainer.innerHTML = `&#x201F${item.slip.advice}&#x201D`
-        img.classList.remove('rotate')
+        // img.classList.remove('rotate')
     })
 }
 getQuote()
